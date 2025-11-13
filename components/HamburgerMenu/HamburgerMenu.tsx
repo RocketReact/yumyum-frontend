e1199bb0b0044daaf8ba21e823dd714e4834ecf6
Oleg Shapoval <olegkazhan@gmail.com>
2025-11-13 22:09:49 +0200

    cursor pointer burger

--- components/HamburgerMenu/HamburgerMenu.tsx
+++ components/HamburgerMenu/HamburgerMenu.tsx
@@ -1,6 +1,7 @@
+import css from '../Header/Header.module.css';
 export default function HamburgerMenu() {
   return (
-    <div>
+    <div className={css.burger}>
       <svg stroke="var(--white)" width={32} height={32}>
         <use href="/sprite.svg#Genericburger" />
       </svg>
