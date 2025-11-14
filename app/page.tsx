01417869ceeec98eb4d441fa4150dc5e8370f1a7
Artem Vernyhora <artemvernyhora8@icloud.com>
2025-11-14 18:51:06 +0200

    create recipesList

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
 import Image from 'next/image';
 import styles from './page.module.css';
+import RecipesList from '@/components/RecipesList/RecipesList';
 
 export default function Home() {
-  return <div className={styles.page}>Main page</div>;
+  return (
+    <>
+      <div className={styles.page}>Main page</div>
+      <RecipesList />
+    </>
+  );
 }
---
+++ types/recipe.ts
@@ -0,0 +1,20 @@
+export interface RecipeIngredient {
+  id: string;
+  measure?: string;
+}
+
+export interface Recipe {
+  _id: string;
+  title: string;
+  category: string;
+  area?: string;
+  instructions: string;
+  description?: string;
+  thumb?: string;
+  time: number;
+  cals?: number;
+  owner: string;
+  ingredients: RecipeIngredient[];
+  createdAt: string;
+  updatedAt: string;
+}
