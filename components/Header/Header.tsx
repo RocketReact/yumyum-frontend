736271b0eae26209730589651c766f7e099cb052
Oleg Shapoval <olegkazhan@gmail.com>
2025-11-14 13:06:58 +0200

    use Container

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -6,35 +6,44 @@ import css from './Header.module.css';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import HamburgerMenu from '@/components/HamburgerMenu/HamburgerMenu';
+import Container from '../Container/Container';
 
 export default function Header() {
   const pathURL = usePathname();
   if (!pathURL || pathURL.startsWith('/auth')) return null;
   return (
     <header className={css.header}>
-      <nav className={css.headerNav}>
-        <Link href="/">
-          <Image src="/logo.svg" width={165} height={46} alt="Logo" priority />
-        </Link>
-        <button
-          className={css.burger}
-          area-laber="Open menu"
-          type="button"
-          aria-controls="app-sidebar"
-          //          aria-expanded={open}
-        >
-          <HamburgerMenu />
-        </button>
-        {/* <button
+      <Container>
+        <nav className={css.headerNav}>
+          <Link href="/">
+            <Image
+              src="/logo.svg"
+              width={165}
+              height={46}
+              alt="Logo"
+              priority
+            />
+          </Link>
+          <button
+            className={css.burger}
+            area-laber="Open menu"
+            type="button"
+            aria-controls="app-sidebar"
+            //          aria-expanded={open}
+          >
+            <HamburgerMenu />
+          </button>
+          {/* <button
             type="button"
             className={css.burgerBtn}
             area-laber="Open menu"
             aria-controls="app-sidebar"
             aria-expanded={open}
           > */
-        /* <GiHamburgerMenu className={css.burgerIcon} size={32} /> */}
-        {/* </button> */}
-      </nav>
+          /* <GiHamburgerMenu className={css.burgerIcon} size={32} /> */}
+          {/* </button> */}
+        </nav>
+      </Container>
     </header>
   );
 }
