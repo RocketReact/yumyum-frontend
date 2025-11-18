a6359b0c03768c2ebd33da6778bc0586e0cacf84
vitalii_cherukha <vitalii.cherukha@gmail.com>
2025-11-18 23:55:34 +0200

    add styles in component recipeDetails

--- components/RecipeDetails/RecipeDetails.tsx
+++ components/RecipeDetails/RecipeDetails.tsx
@@ -1,8 +1,18 @@
 import Container from '../Container/Container';
 import { Recipe } from '@/types/recipe';
 import css from './RecipeDetails.module.css';
+import { getIngredients } from '@/lib/api/clientApi';
 
-const RecipeDetails = ({ recipe }: { recipe: Recipe }) => {
+const RecipeDetails = async ({ recipe }: { recipe: Recipe }) => {
+  const ingredients = await getIngredients();
+
+  const ingredientsMap = new Map(ingredients.map((ing) => [ing._id, ing]));
+
+  const getIngredientName = (id: string): string => {
+    const ingredient = ingredientsMap.get(id);
+    return ingredient?.name || 'Unknown ingredient';
+  };
+
   //   const handleFavorite = async () => {
   //     setLoading(true);
 
@@ -18,7 +28,7 @@ const RecipeDetails = ({ recipe }: { recipe: Recipe }) => {
   //       }
   //     } catch (error) {
   //       console.error('Error toggling favorite:', error);
-  //     } finally {
+  //     } finally
   //       setLoading(false);
   //     }
   //   };
@@ -26,31 +36,52 @@ const RecipeDetails = ({ recipe }: { recipe: Recipe }) => {
   return (
     <section>
       <Container>
-        <h2 className={css.title}>{recipe.title}</h2>
-        <div className={css.imgWrapper}>
-          <img src={recipe.thumb} alt={recipe.title} />
+        <div className={css.titleWrapper}>
+          <div className={css.imgWrapper}>
+            {/* <picture>
+              <source
+                media="(min-width: 768px)"
+                srcSet={recipe.thumb?.replace('preview', 'preview/large')}
+              />
+              <img
+                className={css.titleImg}
+                src={recipe.thumb}
+                alt={recipe.title}
+              />
+            </picture> */}
+            <img
+              className={css.titleImg}
+              src={recipe.thumb}
+              alt={recipe.title}
+            />
+          </div>
+          <h2 className={css.title}>{recipe.title}</h2>
         </div>
-        <div className={css.generalInfoWrapper}>
-          <h3>General information</h3>
-          <div className={css.generalInfoWrapper}>
-            <ul className={css.generalInfoList}>
-              <li className={css.generalInfoItem}>
-                Category:
-                <span className={css.generalInfoSpan}>{recipe.category}</span>
-              </li>
-              <li className={css.generalInfoItem}>
-                Cooking time:
-                <span className={css.generalInfoSpan}>
-                  {recipe.time} minutes
-                </span>
-              </li>
-              <li className={css.generalInfoItem}>
-                Caloric content:
-                <span className={css.generalInfoSpan}>
-                  Approximately {recipe.cals} kcal per serving
-                </span>
-              </li>
-            </ul>
+        <div className={css.infoWrapper}>
+          <div className={css.generalInfoContainer}>
+            <div className={css.generalInfoWrapper}>
+              <h3 className={css.generalInfoTitle}>General information</h3>
+              <ul className={css.generalInfoList}>
+                <li className={css.generalInfoItem}>
+                  Category:&nbsp;
+                  <span className={css.generalInfoSpan}>{recipe.category}</span>
+                </li>
+                <li className={css.generalInfoItem}>
+                  Cooking time:&nbsp;
+                  <span className={css.generalInfoSpan}>
+                    {recipe.time} minutes
+                  </span>
+                </li>
+                <li className={css.generalInfoItem}>
+                  Caloric content:&nbsp;
+                  <span className={css.generalInfoSpan}>
+                    Approximately {recipe.cals} kcal per serving
+                  </span>
+                </li>
+              </ul>
+            </div>
+            {/* //! заглушка */}
+            <button className={css.btn}>Save</button>
             {/* <button
               className={css.generalInfoBtn}
               onClick={handleFavorite}
@@ -73,24 +104,28 @@ const RecipeDetails = ({ recipe }: { recipe: Recipe }) => {
               )}
             </button> */}
           </div>
-        </div>
-        <div className={css.aboutWrapper}>
-          <h3 className={css.aboutTitle}>About recipe</h3>
-          <p className={css.aboutDescription}>{recipe.description}</p>
-        </div>
-        <div className={css.ingredientsWrapper}>
-          <h3>Ingredients:</h3>
-          <ul className={css.ingredientsList}>
-            {recipe.ingredients.map((ingredient) => (
-              <li key={ingredient.id} className={css.ingredientItem}>
-                {ingredient.measure}
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div className={css.preparationWrapper}>
-          <h3 className={css.preparationTitle}>Preparation Steps:</h3>
-          <p className={css.preparationDescription}>{recipe.instructions}</p>
+          <div className={css.otherInfoWrapper}>
+            <div className={css.aboutWrapper}>
+              <h3 className={css.aboutTitle}>About recipe</h3>
+              <p className={css.aboutDescription}>{recipe.description}</p>
+            </div>
+            <div className={css.ingredientsWrapper}>
+              <h3 className={css.ingredientsTitle}>Ingredients:</h3>
+              <ul className={css.ingredientsList}>
+                {recipe.ingredients.map((ingredient) => (
+                  <li key={ingredient.id} className={css.ingredientItem}>
+                    • {getIngredientName(ingredient.id)} —{ingredient.measure}
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div className={css.preparationWrapper}>
+              <h3 className={css.preparationTitle}>Preparation Steps:</h3>
+              <p className={css.preparationDescription}>
+                {recipe.instructions}
+              </p>
+            </div>
+          </div>
         </div>
       </Container>
     </section>
