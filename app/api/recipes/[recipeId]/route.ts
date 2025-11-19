596c3466dd9f9a7c69ea0dc6337fc024112b3530
Egor <qugar04@gmail.com>
2025-11-19 19:28:58 +0200

    add-routes-post-delete

--- app/api/recipes/[recipeId]/route.ts
+++ app/api/recipes/[recipeId]/route.ts
@@ -33,3 +33,51 @@ export async function GET(request: Request, { params }: Params) {
     );
   }
 }
+
+export async function POST(request: Request, { params }: Params) {
+  try {
+    const { recipeId } = await params;
+
+    const res = await api.post<Recipe>(`/recipes/favorites/${recipeId}`);
+
+    return NextResponse.json(res.data, { status: res.status });
+  } catch (error) {
+    if (isAxiosError(error)) {
+      logErrorResponse(error.response?.data);
+      return NextResponse.json(
+        { error: error.message, response: error.response?.data },
+        { status: error.response?.status ?? 500 },
+      );
+    }
+
+    logErrorResponse({ message: (error as Error).message });
+    return NextResponse.json(
+      { error: 'Internal Server Error' },
+      { status: 500 },
+    );
+  }
+}
+
+export async function DELETE(request: Request, { params }: Params) {
+  try {
+    const { recipeId } = await params;
+
+    const res = await api.delete(`/recipes/favorites/${recipeId}`);
+
+    return NextResponse.json(res.data, { status: res.status });
+  } catch (error) {
+    if (isAxiosError(error)) {
+      logErrorResponse(error.response?.data);
+      return NextResponse.json(
+        { error: error.message, response: error.response?.data },
+        { status: error.response?.status ?? 500 },
+      );
+    }
+
+    logErrorResponse({ message: (error as Error).message });
+    return NextResponse.json(
+      { error: 'Internal Server Error' },
+      { status: 500 },
+    );
+  }
+}
